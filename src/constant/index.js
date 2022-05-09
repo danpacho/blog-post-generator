7d2e2dfd7fe4b5453c57840ba9ac557ef62a978a
June@MacBook <danpa725@cau.ac.kr>
2022-05-09 17:17:24 +0900

    Add: constant .

--- src/constant/index.js
+++ src/constant/index.js
@@ -3,5 +3,9 @@ const POSTING_TYPE = {
     CURRENT: "Current Category",
     NEW: "New Category",
 };
+const BLOG_FOLDER_NAME = {
+    CONTENTS: "contents",
+    POSTS: "posts",
+};
 
-export { MAC_OS_FILE_EXCEPTION, POSTING_TYPE };
+export { MAC_OS_FILE_EXCEPTION, POSTING_TYPE, BLOG_FOLDER_NAME };
