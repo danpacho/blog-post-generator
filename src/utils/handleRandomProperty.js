b0df1054ca331e256184555d5d392669ec868432
June@MacBook <danpa725@cau.ac.kr>
2022-05-10 00:42:54 +0900

    Fix: generate correct HEX problem .

--- src/utils/handleRandomProperty.js
+++ src/utils/handleRandomProperty.js
@@ -1,6 +1,9 @@
 //* generate random property
 const generateID = () => (Math.random() + 1).toString(36).slice(7);
+
 const generateHEX = () =>
-    `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    `#${Math.floor(Math.random() * (0xffffff + 1))
+        .toString(16)
+        .padStart(6, "0")}`;
 
 export { generateID, generateHEX };
