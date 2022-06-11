113ce7a87afad31d121029aadebd68e24efaf845
June@MacBook <danpa725@cau.ac.kr>
2022-06-12 00:36:40 +0900

    Fix: constant type assertion .

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,20 +1,56 @@
+/**
+ * @type {".DS_Store"}
+ */
 const MAC_OS_FILE_EXCEPTION = ".DS_Store";
+
+/**
+ * @typedef POSTING
+ * @property CURRENT {"Current Category"}
+ * @property NEW {"New Category"}
+ */
+
+/**
+ * @type POSTING
+ */
 const POSTING_TYPE = {
     CURRENT: "Current Category",
     NEW: "New Category",
 };
+
+/**
+ * @typedef BLOG_FOLDER_NAME_TYPE
+ * @property CONTENTS {"contents"}
+ * @property POSTS {"posts"}
+ */
+
+/**
+ * @type {BLOG_FOLDER_NAME_TYPE}
+ */
 const BLOG_FOLDER_NAME = {
     CONTENTS: "contents",
     POSTS: "posts",
 };
-const DESCRIPTION_FILE_TYPE = {
+
+/**
+ * @typedef DESCRIPTION_FILE_TYPE
+ * @property JSON {"json"}
+ * @property TXT {"txt"}
+ */
+
+/**
+ * @type {DESCRIPTION_FILE_TYPE}
+ */
+const DESCRIPTION_FILE = {
     JSON: "json",
     TXT: "txt",
 };
+
+const GENERATION_TIME = 750;
 
 export {
     MAC_OS_FILE_EXCEPTION,
     POSTING_TYPE,
     BLOG_FOLDER_NAME,
-    DESCRIPTION_FILE_TYPE,
+    DESCRIPTION_FILE,
+    GENERATION_TIME,
 };
